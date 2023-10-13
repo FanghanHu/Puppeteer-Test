a5248dfa6b1b27c2a3f12dac05fb53efa9760c81
Fanghan Hu <fanghan.hu@gmail.com>
2023-10-13 10:46:38 -0500

    use the pages hosted on github pages

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ import puppeteer from "puppeteer";
 (async () => {
     // Launch the browser and open a new blank page
     const browser = await puppeteer.launch(
-        {headless: false}
-        // {headless: 'new'}
+        // {headless: false}
+        {headless: 'new'}
     )
     let page = await browser.newPage();
 
     // Navigate the page to a URL
-    await page.goto("http://127.0.0.1:5500/index.html");
+    await page.goto("https://fanghanhu.github.io/Puppeteer-Test/");
     // Set screen size
     await page.setViewport({ width: 1080, height: 1024 });
 
